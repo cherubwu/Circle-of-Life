7ca2df6651a358b777738f2ac78c9f61fd9aacae
Cherub Woo <cherubwu@Cherubs-MacBook-Pro-10.local>
2026-04-12 14:11:35 +0100

    fix rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
-import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  return rss({
+    title: typeof SITE_TITLE === 'string' ? SITE_TITLE : '代谢前沿',
+    description: typeof SITE_DESCRIPTION === 'string' ? SITE_DESCRIPTION : '生物科技洞见平台',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.id}/`,
+    })),
+  });
 }
