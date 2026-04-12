558fd37fa535832dc443b7e8780b9a75ccf4b4d9
Cherub Woo <cherubwu@Cherubs-MacBook-Pro-10.local>
2026-04-12 14:34:36 +0100

    fix base url case

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://cherubwu.github.io',
-  base: '/circle-of-life',
+  base: '/Circle-of-Life',
   integrations: [mdx(), sitemap()],
   i18n: {
     defaultLocale: 'zh',
